49e6b3ba789b25ffe01afbe832653570309cf863
Dave Miriyala <miriyalad@dcsdk12.org>
2016-09-09 12:54:50 -0600

    Fixed all issues

--- quiz.py
+++ quiz.py
@@ -15,47 +15,58 @@ print has_teen(11, 13, 17)
 print has_teen(21, 23, 27)
 print has_teen(17, 22, 25)
 
+def not_string(str):
+	if str[0:3] == "not":
+		return str + "not"
+	return "not" + str
+print not_string("a joke")
 
 
 
-def icy_hot(a, b, c):
-	if a < 0:
+def icy_hot(a, b):
+	if a > 100 and b < 0:
 		return True
-	if b > 100:
+	if a < 0 and b > 100:
 		return True
-	if c > 100 or c < 0:
-		return True
-	else:
+	else: 
 		return False
-print icy_hot(1, 2, 3)
-print icy_hot(100, 200, 300)
+print icy_hot(100, 0)
+print icy_hot(100, 0)
+print icy_hot(101, -1)
+print icy_hot(101, 12)
+print icy_hot(10, 123)
 
 
-def closer_to(a):
-	
-	if a > 5:
-		return True
-	if a < 5:
-		return False
-	
-print closer_to(5)
-print closer_to(9)
-print closer_to(15)
+def closer_to(a, b, c):
+	guess1 = a - b
+	guess2 = a - c
+	if abs(guess1) > abs(guess2):
+		return c
+	if abs(guess1) < abs(guess2):
+		return b
+	if abs(guess1) == abs(guess2):
+		return 0
+print closer_to(321, 123, 234)
+print closer_to(321, 323, 12)
+print closer_to(321, 321, 321)
+print closer_to(321, 319, 323)
 
 
 
 def two_as_one(a, b, c):
-	if a + b > c:
+	if a + b == c:
 		return True
-	if a + c > b:
+	if b + c == a:
 		return True
-	if b + c > a:
+	if a + c == b:
+		return True
+	else:
 		return False
 	
 		
 print two_as_one(1, 2, 3)
-print two_as_one(2, 4, 6)
-print two_as_one(1, 3, 7)
-print two_as_one(1, 2, 4)
+print two_as_one(1, 3, 2)
+print two_as_one(3, 1, 2)
+print two_as_one(3, 1, 56)
 
 
